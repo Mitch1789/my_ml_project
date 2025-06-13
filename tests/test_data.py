896f5de413a0abc5324e2a54015b00d2679b22cd
Miguel Benitez <mitchbenitez@gmail.com>
2025-06-12 20:32:11 -0400

    Update test_data to use sklearn.load_iris instead of checking data/ folder

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,10 @@
-import os
+from sklearn.datasets import load_iris
 
 
-def test_data_exists():
-    assert os.path.isdir("data"), "data/ directory is missing"
+def test_load_iris():
+    """
+    Ensure that the Iris dataset can be loaded via scikit-learn.
+    """
+    X, y = load_iris(return_X_y=True)
+    assert X.shape[0] > 0 and X.shape[1] > 0
+    assert len(y) > 0
