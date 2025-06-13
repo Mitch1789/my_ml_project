7882318abf445d99453aecac54ae78eec4b0a3f0
Miguel Benitez <mitchbenitez@gmail.com>
2025-06-12 21:01:00 -0400

    Comment out intentional failing code to pass the pipeline

--- tests/test_force_fail.py
+++ tests/test_force_fail.py
@@ -1,5 +1,5 @@
-# def test_force_ci_failure():
-#     """
-#     This test is here to intentionally fail the CI pipeline.
-#     """
-#     assert False, "💥 Intentional CI failure for testing purposes"
+def test_force_ci_failure():
+    """
+    This test is here to intentionally fail the CI pipeline.
+    """
+    assert False, "💥 Intentional CI failure for testing purposes"
